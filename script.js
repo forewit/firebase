3eecb4208607d20283587c8b5e54806c15d23c35
Marc Anderson <marcda495@gmail.com>
2020-06-04 23:32:16 -0400

    na

--- script.js
+++ script.js
@@ -20,21 +20,64 @@ let ref = database.ref("data/");
 // READ from realtime db
 
 // value event triggers on every change
-ref.on('value', function(snapshot) { 
+ref.on('value', function (snapshot) {
     console.log("updated value: ", snapshot.val());
 });
 
 // read once
-ref.once('value').then(function(snapshot) {
+ref.once('value').then(function (snapshot) {
     console.log("read value once: ", snapshot.val());
 });
 
+
 // -----------------------------
 // WRITE to realtime db
-ref.set({name:"calendar"}, function(error) {
+ref.set({ name: "calendar" }, function (error) {
     if (error) {
         console.log("write failed!");
     } else {
         console.log("write successful!");
     }
 });
+
+
+// -----------------------------
+// AUTHENTICATION
+let ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// Auth UI configuration
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            console.log(authResult);
+            return true;
+        },
+        uiShown: function () {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        //firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
